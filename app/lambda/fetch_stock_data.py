88c057975ee6702ec0b91281c2f9ba67b69a9c63
ad1tyagoyal <adityagoyal867@gmail.com>
2025-06-20 00:51:20 +0530

    initial raw data fetching lambda implementation

--- app/lambda/fetch_stock_data.py
+++ app/lambda/fetch_stock_data.py
@@ -1,5 +1,7 @@
 import yfinance as yf # type: ignore
+from  app.utils.constants import nifty50_stocks
 
-tcs = yf.Ticker("TCS.NS")
-data = tcs.history(period="6mo") # type: ignore
-print(data)
+for stock in nifty50_stocks:
+    tcs = yf.Ticker(stock)
+    data = tcs.history(period="6mo") # type: ignore
+    print(data)
